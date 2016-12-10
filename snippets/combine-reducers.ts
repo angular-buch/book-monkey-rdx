76c2346402ab583adf97297bc5f1c5f0b7d51fbc
Gregor Woiwode <gw@co-it.eu>
2016-12-10 13:59:20 +0100

    Adds snippet combining reducers

---
+++ snippets/combine-reducers.ts
@@ -0,0 +1,6 @@
+import	{	combineReducers	}	from	'redux';
+
+import { aReducer as a}	from	'./a-reducer';
+import { bReducer as b }	from	'./b-reducer';
+
+export const reducer = combineReducers({ a, b });
