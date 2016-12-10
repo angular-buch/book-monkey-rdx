0e73234980b4924c4e54b9adde2e921c2fe0f48a
Gregor Woiwode <gw@co-it.eu>
2016-12-10 14:44:49 +0100

    Fixes action creater service snippet

--- snippets/action-creator-service-dep.ts
+++ snippets/action-creator-service-dep.ts
@@ -7,12 +7,12 @@ import { BookStoreService } from '../shared/book-store.service';
 @Injectable()
 export class	BookActions	{
   constructor(
-    private	redux: NgRedux<number>,
+    private	redux: NgRedux<any>,
     private bs: BookStoreService)	{ }
 
   getBooks() {
     this.bs.getAll().subscribe((books: Book[]) => {
-      this.redux.dispatch({ type: 'INCREMENT_COUNT', playload: books });
+      this.redux.dispatch({ type: 'BOOKS_LOADED', playload: books });
     });
   }
 }
