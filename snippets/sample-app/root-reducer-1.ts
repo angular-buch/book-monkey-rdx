506f320ab44c6b41d8dc05ff00f9620c2d792270
Gregor Woiwode <gw@co-it.eu>
2017-01-23 01:36:47 +0100

    Adds snippet for first reducer registration

---
+++ snippets/sample-app/root-reducer-1.ts
@@ -0,0 +1,8 @@
+import { combineReducers	} from 'redux';
+
+import { IAppState } from './../app.state';
+import { booksReducer as books}	from './books';
+
+export const reducer = combineReducers<IAppState>({
+  books
+});
