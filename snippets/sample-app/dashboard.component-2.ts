372ff2f5c9270b0eebbac952462b79fd38b17921
Gregor Woiwode <gw@co-it.eu>
2017-01-24 01:09:42 +0100

    Adds snippets with CardActions for dashboard

---
+++ snippets/sample-app/dashboard.component-2.ts
@@ -0,0 +1,21 @@
+/* ... */
+
+import { BooksActions, CardActions } from '../../actions';
+
+@Component({
+  selector: 'bm-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DashboardComponent {
+
+  constructor(private books: BooksActions,
+              private card: CardActions) { }
+
+  /* ... */
+
+  addBookToCard(book: Book) {
+    this.card.createSingle(book);
+  }
+}
