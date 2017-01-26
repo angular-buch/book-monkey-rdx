535e84d937bfaacc6682640887cfd31bc8a9f139
Gregor Woiwode <gw@co-it.eu>
2017-01-27 00:41:35 +0100

    Adds snippet for redux-dev-tools

---
+++ snippets/power-tipp/register-dev-tools.ts
@@ -0,0 +1,29 @@
+// ...
+import { NgReduxModule, DevToolsExtension, NgRedux } from 'ng2-redux';
+
+
+@NgModule({
+  // ...
+  imports: [
+    // ...
+    NgReduxModule
+  ],
+  // ...
+})
+export class AppModule {
+  constructor(private ngRedux: NgRedux<IAppState>,
+              private devTools: DevToolsExtension) {
+    let enhancers = [];
+
+    if (environment.production === false) {
+      enhancers = [...enhancers, devTools.enhancer()];
+    }
+
+    this.ngRedux.configureStore(
+      reducer,
+      {} as IAppState,
+      [],
+      enhancers
+    );
+  }
+}
